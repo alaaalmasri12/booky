fc67866ec746ac0c35e4ff15feefc9848117b74a
alaaalmasri12 <alaa_almasri1997@yahoo.com>
2023-12-05 07:00:17 +0300

    delete folder

--- Booky/Booky/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Booky.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
